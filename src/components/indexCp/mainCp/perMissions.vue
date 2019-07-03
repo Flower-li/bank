0eb065ff4745d72969e618c816d863d6c5b3d279
li <769752877@qq.com>
2019-07-03 16:57:52 +0800

    1

--- src/components/indexCp/mainCp/perMissions.vue
+++ src/components/indexCp/mainCp/perMissions.vue
@@ -98,9 +98,9 @@
         </div>
         <div class="page">
           <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="currentPage4"
+            @size-change="20"
+            @current-change="1"
+            :current-page="1"
             :page-sizes="[100, 200, 300, 400]"
             :page-size="100"
             layout="total, sizes, prev, pager, next, jumper"
@@ -123,6 +123,7 @@ export default {
   data() {
     return {
       dialogVisible: false,
+      input: "",
       tableData: [
         {
           date: "2016-05-02",
